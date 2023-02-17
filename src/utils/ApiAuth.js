48afc6aefe0ea3d5a7c02c2443904a1e4bfca046
Aleksander Panferov2 <panferov.a.a@yandex.ru>
2023-02-17 15:49:27 +0300

    feat: creat ApiAuth.js

---
+++ src/utils/ApiAuth.js
@@ -0,0 +1,36 @@
+const config = {
+  url: "https://auth.nomoreparties.co",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
+
+const checkResponse = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+};
+
+export const register = (email, password) => {
+  return fetch(`${config.url}/signup`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const authorize = (email, password) => {
+  return fetch(`${config.url}/signin`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const checkToken = (JWT) => {
+  return fetch(`${config.url}/users/me`, {
+    method: "GET",
+    headers: { ...config.headers, Authorization: `Bearer ${JWT}` },
+  }).then(checkResponse);
+};
+
+export default ApiAuth;
